212e9bb062d57d341e942d59e86ae5c17b23cd24
arthur <arthur.broudoux@gmail.com>
2024-01-10 23:53:20 +0100

    change date format

--- items/media.sh
+++ items/media.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sketchybar --add item media right \
-           --set media label.color=$ACCENT_COLOR \
-                       label.max_chars=20 \
+           --set media label.color=$WHITE \
+                       label.max_chars=22 \
                        icon.padding_left=20 \
                        scroll_texts=on \
                        icon=􀑪             \
-                       icon.color=$ACCENT_COLOR   \
+                       icon.color=$WHITE   \
                        background.drawing=off \
                        script="$PLUGIN_DIR/media.sh" \
            --subscribe media media_change
